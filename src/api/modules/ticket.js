20d98251ed2320912419997cc74700e3c5a45a2b
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-03-01 21:52:00 -0600

    close #13

--- src/api/modules/ticket.js
+++ src/api/modules/ticket.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const admin = require("firebase-admin");
+const moment = require('moment')
 const TicketResponse = require('../../models/TicketResponse');
 const ErrorResponse = require("../../models/ErrorResponse");
 const { v4: uuidv4 } = require('uuid');
@@ -22,12 +23,13 @@ router.route("/")
         const tickets = await admin.firestore().collection('tickets').where("userId", "==", res.locals.userId).get()
 
         if (tickets) {
-            return res.status(200).send(new TicketResponse(200, tickets.docs.map(doc => doc.data())))
+            return res.status(200).send(new TicketResponse(200, formatTickets(tickets)))
         }
 
         return res.status(200).send(new TicketResponse(200, []))
 
     } catch (err) {
+        console.log(err)
         return res.status(500).send(new ErrorResponse(500, "Something went wrong..." + err.message))
     }
 })
@@ -40,6 +42,7 @@ router.route("/")
         const ticket = { 
             userId: res.locals.userId, 
             ticketId: uuidv4().substring(0, 20),
+            created_at: new Date(),
             ...req.body,
         }
         await admin.firestore().collection('tickets').doc().set(ticket)
@@ -49,4 +52,15 @@ router.route("/")
         return res.status(500).send(new ErrorResponse(500, err.message))
     }
 })
+
+
+const formatTickets = tickets => {
+    return tickets.docs.map(doc => {
+        const data = doc.data();
+        data.created_at = moment(new Date(data.created_at._seconds * 1000 + data.created_at._nanoseconds/1000000)).format('MM/DD/YYYY')
+        return data
+    })
+}
+
+
 module.exports = router;

04b71b871c5b6ccba7d7ec69d759a2a0e6ed46c9
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-03-10 12:25:12 -0600

    close #12

--- src/api/modules/ticket.js
+++ src/api/modules/ticket.js
@@ -9,55 +9,82 @@ const router = express.Router()
 
 router.use((req, res, next) => {
     admin.auth()
-    .verifyIdToken(req.headers.token)
-    .then(decodedToken => {
-        res.locals.userId = decodedToken.uid
-        next()
-    })
-    .catch(_ => res.status(401).send(new ErrorResponse(401, "Invalid ID token.")))
+        .verifyIdToken(req.headers.token)
+        .then(decodedToken => {
+            res.locals.userId = decodedToken.uid
+            next()
+        })
+        .catch(_ => res.status(401).send(new ErrorResponse(401, "Invalid ID token.")))
 })
 
 router.route("/")
-.get( async (req, res) => {
-    try {
-        const tickets = await admin.firestore().collection('tickets').where("userId", "==", res.locals.userId).get()
+    .get(async (req, res) => {
+        try {
+            const tickets = await admin.firestore().collection('tickets').where("userId", "==", res.locals.userId).get()
 
-        if (tickets) {
-            return res.status(200).send(new TicketResponse(200, formatTickets(tickets)))
-        }
+            if (tickets) {
+                return res.status(200).send(new TicketResponse(200, formatTickets(tickets)))
+            }
 
-        return res.status(200).send(new TicketResponse(200, []))
+            return res.status(200).send(new TicketResponse(200, []))
 
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send(new ErrorResponse(500, "Something went wrong..." + err.message))
-    }
-})
-.post (async (req, res) => {
-    try {
-        const _status = req.body.hasStatus.toUpperCase()
-        if (_status !== "OPEN") {
-            return res.status(400).send(new ErrorResponse(400, "Invalid status."))
+        } catch (err) {
+            console.log(err)
+            return res.status(500).send(new ErrorResponse(500, "Something went wrong..." + err.message))
         }
-        const ticket = { 
-            userId: res.locals.userId, 
-            ticketId: uuidv4().substring(0, 20),
-            created_at: new Date(),
-            ...req.body,
+    })
+    .post(async (req, res) => {
+        try {
+            const _status = req.body.hasStatus.toUpperCase()
+            if (_status !== "OPEN") {
+                return res.status(400).send(new ErrorResponse(400, "Invalid status."))
+            }
+            const ticketId = uuidv4().substring(0, 23).split("-").join("")
+            const ticket = {
+                ...req.body,
+                userId: res.locals.userId,
+                ticketId: ticketId,
+                created_at: new Date(),
+            }
+            await admin.firestore().collection('tickets').doc(ticketId).set(ticket)
+            return res.status(201).send({ ticket })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).send(new ErrorResponse(500, err.message))
         }
-        await admin.firestore().collection('tickets').doc().set(ticket)
-        return res.status(201).send({ ticket })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send(new ErrorResponse(500, err.message))
-    }
-})
+    })
+    .patch(async (req, res) => {
+        try {
+            const doc = await admin.firestore().collection('tickets').doc(req.body.ticketId).get()
+            if (!doc.exists) {
+                return res.status(404).send(new ErrorResponse(404, `No ticket with ID ${req.body.ticketId} exists.`))
+            }
+
+            const { userId } = doc.data()
+
+            if (userId !== res.locals.userId) {
+                return res.status(401).send(new ErrorResponse(401, "Unauthorized."))
+            }
+
+            await admin.firestore().collection('tickets').doc(req.body.ticketId).update({
+                base_pay: req.body.basePay,
+                rate_per_mile: req.body.ratePerMile,
+                description: req.body.description,
+                hasStatus: req.body.hasStatus,
+                total: req.body.total
+            }, { merge: true })
+            return res.status(200).send(true)
+        } catch (err) {
+            console.log(err)
+            return res.status(500).send(new ErrorResponse(500, err.message))
+        }
+    })
 
 
 const formatTickets = tickets => {
     return tickets.docs.map(doc => {
         const data = doc.data();
-        data.created_at = moment(new Date(data.created_at._seconds * 1000 + data.created_at._nanoseconds/1000000)).format('MM/DD/YYYY')
+        data.created_at = moment(new Date(data.created_at._seconds * 1000 + data.created_at._nanoseconds / 1000000)).format('MM/DD/YYYY')
         return data
     })
 }

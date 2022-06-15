b96070af86214b8bf3dae96823d45ca0c6bdd43f
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-06-15 13:21:23 -0500

    history route

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 const accountApi = require('./modules/account')
 const ticketApi = require('./modules/ticket');
+const historyApi = require('./modules/history')
 
 const ROUTE_PREFIX = "/api/v1/"
 
 const api = app => {
     app.use(ROUTE_PREFIX + "account", accountApi),
-    app.use(ROUTE_PREFIX + "tickets", ticketApi)
+    app.use(ROUTE_PREFIX + "tickets", ticketApi),
+    app.use(ROUTE_PREFIX + "history", historyApi)
 }
 
 module.exports = api
